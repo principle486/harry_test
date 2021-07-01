7fc4404cbe42392fcb9d965408717dc9348955cd
Song Jun Yong <principle345@gmail.com>
2021-07-01 22:16:02 +0900

    BaseTimeEntity Study

---
+++ src/main/java/com/harry/admin/springboot/web/dto/PostsSaveRequestDto.java
@@ -0,0 +1,30 @@
+package com.harry.admin.springboot.web.dto;
+
+import com.harry.admin.springboot.web.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class PostsSaveRequestDto {
+
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostsSaveRequestDto(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity() {
+        return Posts.builder()
+                    .title(title)
+                    .content(content)
+                    .author(author)
+                    .build();
+    }
+}
