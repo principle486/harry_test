5066de6af8d1123d87023d77d34e6f05a68c4461
Song Jun Yong <principle345@gmail.com>
2021-07-05 07:59:00 +0900

    Study 3

--- src/main/java/com/harry/admin/springboot/service/PostsService.java
+++ src/main/java/com/harry/admin/springboot/service/PostsService.java
@@ -2,13 +2,16 @@ package com.harry.admin.springboot.service;
 
 import com.harry.admin.springboot.web.domain.posts.Posts;
 import com.harry.admin.springboot.web.domain.posts.PostsRepository;
+import com.harry.admin.springboot.web.dto.PostsListResponseDto;
 import com.harry.admin.springboot.web.dto.PostsResponseDto;
 import com.harry.admin.springboot.web.dto.PostsSaveRequestDto;
 import com.harry.admin.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +37,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAlldesc().stream()
+                                            .map(PostsListResponseDto::new)
+                                            .collect(Collectors.toList());
     }
 }
